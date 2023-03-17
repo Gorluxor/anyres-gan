53a203526bb597ba64db94e8f503cb1ce8451cf2
Gorluxor <18031411+Gorluxor@users.noreply.github.com>
2023-03-17 20:42:37 +0300

    fixed display of DG conditoning

--- train.py
+++ train.py
@@ -394,7 +394,9 @@ def main(**kwargs):
         c.cudnn_benchmark = False
 
     # Description string.
-    extra_str = 'D' if opts.bcond else '' + 'G' if opts.bcondg else ''
+    extra_str1 = 'D' if opts.bcond else ''
+    extra_str2 = 'G' if opts.bcondg else ''
+    extra_str = f'{extra_str1}{extra_str2}'
     desc = f'{opts.cfg:s}-{dataset_name:s}-gpus{c.num_gpus:d}-batch{c.batch_size:d}-gamma{c.loss_kwargs.r1_gamma:g}--teacher{opts.teacher_lambda:.1f}{extra_str}'
     if opts.desc is not None:
         desc += f'-{opts.desc}'
